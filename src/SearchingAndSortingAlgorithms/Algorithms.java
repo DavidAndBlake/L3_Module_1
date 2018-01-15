3d37e551d2fe8b1529636c104d4773c0d9fc7b85
League <league@WTS8.attlocal.net>
2018-01-14 17:31:05 -0800

    1/14/18

--- src/SearchingAndSortingAlgorithms/Algorithms.java
+++ src/SearchingAndSortingAlgorithms/Algorithms.java
@@ -57,5 +57,18 @@ public class Algorithms {
 		return false;
 	}
 
+	public static List<Double> sortScores(List<Double> results) {
+		for (int i = 0; i < results.size(); i++) {
+			for (int j = i + 1; j < results.size(); j++) {
+				if (results.get(i) > results.get(j)) {
+					double temp = results.get(i);
+					results.set(i, results.get(j));
+					results.set(j, temp);
+				}
+			}
+		}
+		return results;
+	}
+
 	// Add other methods here
 }
